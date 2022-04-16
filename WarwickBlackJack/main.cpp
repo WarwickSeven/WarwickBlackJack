8fa23fae5f9dbecc14a95442cafc10c73a14ee4a
WarwickSeven <WarwickSeven@gmail.com>
2022-04-16 17:19:27 +0300

    clear main

--- WarwickBlackJack/main.cpp
+++ WarwickBlackJack/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  WarwickBlackJack
-//
-//  Created by Александр Тихонов on 16.04.2022.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
